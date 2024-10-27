4ba177c370568dad48f0b68f07254efe077b00d3
Polina <gulienko.polina@mail.ru>
2024-10-27 14:08:06 +0500

    Task1

--- Tyuiu.GulienkoPO.Sprint3.Task1.V5.Test/DataServiceTest.cs
+++ Tyuiu.GulienkoPO.Sprint3.Task1.V5.Test/DataServiceTest.cs
@@ -9,15 +9,14 @@ namespace Tyuiu.GulienkoPO.Sprint3.Task1.V5.Test
         public void ValidGetMultiplySeries()
         {
             DataService ds = new DataService();
-
             int startValue = 1;
-            int endValue = 10;
-
-            double res = ds.GetMultiplySeries(startValue, endValue);
-
+            int stopValue = 10;
+            double res = ds.GetMultiplySeries(startValue, stopValue);
             double wait = 3.195;
-
             Assert.AreEqual(wait, res);
+
+
+
 
         }
     }
