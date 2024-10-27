413d8cc410532e48cfb5ab9ea1df5456354c5845
Polina <gulienko.polina@mail.ru>
2024-10-27 21:38:35 +0500

    Task2

---
+++ Tyuiu.GulienkoPO.Sprint3.Task2.V8.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GulienkoPO.Sprint3.Task2.V8.Lib
+{
+    public class DataService : ISprint3Task2V8
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double MultiplySeries = 0;
+            do
+            {
+                MultiplySeries = MultiplySeries * (1 / Math.Pow((Math.Cos(5) + 1), 2));
+                startValue++;
+            } while (startValue < stopValue);
+            return Math.Round(MultiplySeries, 3);
+        }
+    }
+}
