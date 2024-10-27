34f2c9f883c1f96648b3e3cd69368cd70f6a217b
Polina <gulienko.polina@mail.ru>
2024-10-27 21:28:31 +0500

    Task2

---
+++ Tyuiu.GulienkoPO.Sprint3.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Tyuiu.GulienkoPO.Sprint3.Task2.v8.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint3.Task2.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 15;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = 0;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.GulienkoPO.Sprint3.Task2.V8/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.GulienkoPO.Sprint3.Task2.v8.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint3.Task2.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 15;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда: " + Math.Round(ds.GetMultiplySeries(startValue, stopValue), 3));
+
+            Console.ReadKey();
+        }
+    }
+}
