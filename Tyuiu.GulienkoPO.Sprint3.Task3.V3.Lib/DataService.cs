75ef6356ea6e6445e2e4658e92a2ac8084b88caf
Polina <gulienko.polina@mail.ru>
2024-10-28 13:42:03 +0500

    Task3

--- Tyuiu.GulienkoPO.Sprint3.Task3.V3.Lib/DataService.cs
+++ Tyuiu.GulienkoPO.Sprint3.Task3.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.GulienkoPO.Sprint3.Task3.V3.Lib
         public int GetMinCharCount(string value, char item)
         {
             int count = 0;
-            foreach (char chr  in value)
+            foreach (char chr in value)
             {
                 if (chr == item)
                 {
